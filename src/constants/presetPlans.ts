b649f7a5ffb89ce568b2bd1415671cbef0f55f4b
Marchuk Dmytro <marcukdyma@gmail.com>
2026-07-28 15:56:41 +0200

    changed data

--- src/constants/presetPlans.ts
+++ src/constants/presetPlans.ts
@@ -2,6 +2,7 @@ import { IWorkoutPlan } from "../Types/plan";
 
 export const PRESET_WORKOUT_PLANS: IWorkoutPlan[] = [
    {
+      // Мій старий план:
       id: "fullbody-2day",
       titleKey: "plans.fullbody_2day.title",
       descriptionKey: "plans.fullbody_2day.description",
@@ -11,26 +12,40 @@ export const PRESET_WORKOUT_PLANS: IWorkoutPlan[] = [
             dayId: "fullbody-2day-day1",
             dayNameKey: "plans.fullbody_2day.day1",
             exercises: [
-               { exerciseIndex: 30, sets: 4, reps: "8-10", defaultWeight: 40, proTipKey: "plans.proTip.gobletSquat" },
-               { exerciseIndex: 0, sets: 4, reps: "6-8", defaultWeight: 70, proTipKey: "plans.proTip.benchPress" },
-               { exerciseIndex: 34, sets: 4, reps: "8-10", defaultWeight: 60, proTipKey: "plans.proTip.latPulldown" },
+               // 1. Присідання з гантелею -> "Присідання з гантелею" (індекс 29)
+               { exerciseIndex: 29, sets: 3, reps: "10-12", defaultWeight: 40, proTipKey: "plans.proTip.gobletSquat" },
+               // 2. Жим лежачи -> "Жим лежачи" (індекс 0)
+               { exerciseIndex: 0, sets: 3, reps: "8-10", defaultWeight: 70, proTipKey: "plans.proTip.benchPress" },
+               // 3. Тяга верт. блоку -> "Тяга верхнього блока" (індекс 33)
+               { exerciseIndex: 33, sets: 3, reps: "10-12", defaultWeight: 60, proTipKey: "plans.proTip.latPulldown" },
+               // 4. Розведення гантелей -> "Підйом гантелей у сторони" (індекс 12)
                { exerciseIndex: 12, sets: 3, reps: "12-15", defaultWeight: 10, proTipKey: "plans.proTip.lateralRaise" },
-               { exerciseIndex: 10, sets: 3, reps: "10-12", defaultWeight: 14, proTipKey: "plans.proTip.bicepCurl" },
-               { exerciseIndex: 76, sets: 3, reps: "12-15", defaultWeight: 0, proTipKey: "plans.proTip.hyperextension" },
-               { exerciseIndex: 19, sets: 3, reps: "15-20", defaultWeight: 0, proTipKey: "plans.proTip.crunch" },
+               // 5. Згинання на біцепс -> "Тяга гантелей однією рукою" (індекс 10)
+               { exerciseIndex: 26, sets: 2, reps: "12", defaultWeight: 14, proTipKey: "plans.proTip.bicepCurl" },
+               // 6. Гіперекстензія -> "Гіперекстензія" (індекс 75)
+               { exerciseIndex: 75, sets: 3, reps: "12-15", defaultWeight: 0, proTipKey: "plans.proTip.hyperextension" },
+               // 7. Скручування (Прес) -> "Скручування" (індекс 19)
+               { exerciseIndex: 19, sets: 3, reps: "до паління", defaultWeight: 0, proTipKey: "plans.proTip.crunch" },
             ],
          },
          {
             dayId: "fullbody-2day-day2",
             dayNameKey: "plans.fullbody_2day.day2",
             exercises: [
-               { exerciseIndex: 5, sets: 4, reps: "6-8", defaultWeight: 0, proTipKey: "plans.proTip.pullUp" },
-               { exerciseIndex: 14, sets: 4, reps: "10-12", defaultWeight: 120, proTipKey: "plans.proTip.legPress" },
-               { exerciseIndex: 28, sets: 4, reps: "8-10", defaultWeight: 20, proTipKey: "plans.proTip.dumbbellShoulderPress" },
-               { exerciseIndex: 33, sets: 4, reps: "8-10", defaultWeight: 55, proTipKey: "plans.proTip.seatedCableRow" },
-               { exerciseIndex: 8, sets: 3, reps: "8-10", defaultWeight: 0, proTipKey: "plans.proTip.dip" },
-               { exerciseIndex: 89, sets: 3, reps: "12-15", defaultWeight: 0, proTipKey: "plans.proTip.standingCalfRaise" },
-               { exerciseIndex: 18, sets: 3, reps: "45 sec", defaultWeight: 0, proTipKey: "plans.proTip.plank" },
+               // 1. Турнік (Підтягування) -> "Підтягування" (індекс 5)
+               { exerciseIndex: 5, sets: 3, reps: "макс", defaultWeight: 0, proTipKey: "plans.proTip.pullUp" },
+               // 2. Жим ногами -> "Жим ногами" (індекс 14)
+               { exerciseIndex: 14, sets: 3, reps: "12", defaultWeight: 120, proTipKey: "plans.proTip.legPress" },
+               // 3. Жим гантелей сидячи -> "Жим гантелей сидячи" (індекс 27)
+               { exerciseIndex: 27, sets: 3, reps: "10", defaultWeight: 20, proTipKey: "plans.proTip.dumbbellShoulderPress" },
+               // 4. Тяга горизонт. блоку -> "Тяга нижнього блока" (індекс 34)
+               { exerciseIndex: 34, sets: 3, reps: "12", defaultWeight: 55, proTipKey: "plans.proTip.seatedCableRow" },
+               // 5. Бруси -> "Віджимання на брусах" (індекс 8)
+               { exerciseIndex: 8, sets: 3, reps: "макс", defaultWeight: 0, proTipKey: "plans.proTip.dip" },
+               // 6. Підйом на носки -> "Підйоми на носки стоячи" (індекс 90)
+               { exerciseIndex: 90, sets: 2, reps: "15-20", defaultWeight: 0, proTipKey: "plans.proTip.standingCalfRaise" },
+               // 7. Планка -> "Планка" (індекс 18)
+               { exerciseIndex: 18, sets: 2, reps: "1 хв", defaultWeight: 0, proTipKey: "plans.proTip.plank" },
             ],
          },
       ],
